c769e6cc59d8d0942d0149e4f4ec06d8c12fb039
Saikat Chakraborty <saikatgpae@gmail.com>
2022-08-10 13:17:49 +0530

    Activate the remove all button.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log(localStorage.getItem('books'));
 /* eslint-disable */
 
 if (localStorage.getItem('books') === null) {
@@ -7,18 +6,15 @@ if (localStorage.getItem('books') === null) {
   // Clicking Add button pushing the new entry inside the list
   document.querySelector('#add').addEventListener('click', addNewBook);
 } else {
-  console.log('u used this web before');
-
   const storedBooksList = JSON.parse(localStorage.getItem('books'));
-  console.log(storedBooksList.length);
 
   // printing the local storage
   for (let i = 0; i < storedBooksList.length; i++) {
-    console.log(storedBooksList[i].title, storedBooksList[i].author);
     const book = document.createElement('ul');
     book.className = 'book-menu';
     book.innerHTML = `<li class="book-name list-group-item">${storedBooksList[i].title}</li>
                             <li class="author-name list-group-item">${storedBooksList[i].author}</li>
+                            <li class="index d-none author-name list-group-item">${i}</li>
                             <button type="button" class="remove btn btn-outline-secondary">Remove</button>`;
     document.querySelector('.book-details').appendChild(book);
   }
@@ -28,33 +24,28 @@ if (localStorage.getItem('books') === null) {
   document.querySelector('#add').addEventListener('click', addNewBook);
 }
 
+// REMOVING THE WHOLE LIST
 document.querySelector('#removeAll').addEventListener('click', () => {
   localStorage.clear();
   location.reload();
 });
 
+// ADD NEW BOOK FUNCTION
+
 function addNewBook() {
   // pushing the form values in an array
-
   list.push({
     title: document.getElementById('title').value,
     author: document.getElementById('author').value,
   });
-  // printing the book items
-  const book = document.createElement('ul');
-  book.className = 'book-menu';
-  book.innerHTML = `<li class="book-name list-group-item">${document.getElementById('title').value}</li>
-                        <li class="author-name list-group-item">${document.getElementById('author').value}</li>
-                        <button type="button" class="remove btn btn-outline-secondary">Remove</button>`;
-  document.querySelector('.book-details').appendChild(book);
+
   // storing the array inside the local storage
   localStorage.setItem('books', JSON.stringify(list));
-  const storedBooksList = JSON.parse(localStorage.getItem('books'));
-  console.log(storedBooksList[storedBooksList.length - 1]);
+
+  // Clearing the inputs of form
   document.getElementById('title').value = '';
   document.getElementById('author').value = '';
-  console.log(storedBooksList.length);
-  console.log(list[list.length - 1].title);
+  location.reload();
 }
 
 /* eslint-enable */

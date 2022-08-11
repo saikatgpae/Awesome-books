633494c026dc37255ce08f9fcc2cf28d8c5f320f
Okonjo <okonjogilbert2@gmail.com>
2022-08-11 14:29:20 +0300

    add remove button function

--- main.js
+++ main.js
@@ -2,68 +2,82 @@
 var storedBooksList = JSON.parse(localStorage.getItem('books'));
 console.log(storedBooksList);
 class Book {
-  constructor(title, author, index) {
-    this.title = title;
-    this.author = author;
-    this.index = index;
-  }
+    constructor(title, author, index) {
+        this.title = title;
+        this.author = author;
+        this.index = index;
+    }
 }
 
 class Books {
-  constructor() {
-    this.list = [];
-  }
-
-  add(title, author) {
-    if (Array.isArray(this.list)) {
-      this.list.push(new Book(title, author));
+    constructor() {
+        this.list = [];
     }
-  }
 
-  removeItem(index) {
-    if (Array.isArray(this.list)) {
-      const array = this.list;
-      array.splice(index, 1);
+    add(title, author) {
+        if (Array.isArray(this.list)) {
+            this.list.push(new Book(title, author));
+        }
     }
-  }
+
+    removeItem(index) {
+        if (Array.isArray(this.list)) {
+            const array = this.list;
+            array.splice(index, 1);
+        }
+    }
 }
 
 // Printing the local storage data
 if (localStorage.getItem('books') != null) {
-  for (let i = 0; i < storedBooksList.length; i++) {
-    const book = document.createElement('ul');
-    book.className = 'book-menu';
-    book.innerHTML = `<li class="book-name list-group-item">${storedBooksList[i].title}</li>
+    for (let i = 0; i < storedBooksList.length; i++) {
+        const book = document.createElement('ul');
+        book.className = 'book-menu';
+        book.innerHTML = `<li class="book-name list-group-item">${storedBooksList[i].title}</li>
                         <li class="author-name list-group-item">${storedBooksList[i].author}</li>
                         <li class="index author-name list-group-item">${i}</li>
                         <button type="button" class="remove btn btn-outline-secondary">Remove</button>`;
-    document.querySelector('.book-details').appendChild(book);
-  }
+        document.querySelector('.book-details').appendChild(book);
+    }
 }
 
-
-// ADD NEW BOOK.
-
-document.getElementById('add').addEventListener('click', () => {
-  const books = new Books();
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  books.add(title, author);
-  // console.log(books.list);
-  if (localStorage.getItem('books') === null) {
+// DELETE THE CURRENT ROW OF THE DATDABASE.
+function deleteRow() {
+    const index = this.previousElementSibling.innerHTML;
+    const books = new Books();
+    const booksStored = JSON.parse(localStorage.getItem('books'));
+    books.list = booksStored;
+    books.removeItem(index);
+    // console.log(books.list);
     localStorage.setItem('books', JSON.stringify(books.list));
     location.reload();
-  } else {
-    const newList = storedBooksList.concat(books.list);
-    localStorage.setItem('books', JSON.stringify(newList));
-    location.reload();
-  }
+}
+
+document.querySelectorAll('.remove').forEach((removeButton) => {
+    removeButton.addEventListener('click', deleteRow);
+});
+
+// ADD NEW BOOK.
+document.getElementById('add').addEventListener('click', () => {
+    const books = new Books();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    books.add(title, author);
+    // console.log(books.list);
+    if (localStorage.getItem('books') === null) {
+        localStorage.setItem('books', JSON.stringify(books.list));
+        location.reload();
+    } else {
+        const newList = storedBooksList.concat(books.list);
+        localStorage.setItem('books', JSON.stringify(newList));
+        location.reload();
+    }
 });
 
 // REMOVING THE WHOLE LIST
 document.querySelector('#removeAll').addEventListener('click', () => {
-  localStorage.removeItem('books');
-  location.reload();
+    localStorage.removeItem('books');
+    location.reload();
 });
 
 /* eslint-enable */
